3e443c60048b6bf2b1c886c0f8aca10e8e06b5fa
VladimirGrebenev <grebenev-81@mail.ru>
2023-03-01 16:35:20 +0300

    docker working, but no postgre db - clear, pip freeze flask-migrate

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 COPY wsgi.py wsgi.py
 COPY blog ./blog
-COPY .env .env
 EXPOSE 5000
-CMD ["python", "-m", "flask", "run", "--host=0.0.0.0"]
+CMD ["python", "wsgi.py"]
 
--- blog/configs.py
+++ blog/configs.py
@@ -4,7 +4,7 @@ import os
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/blog.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "abcdefg123456"
 
@@ -13,6 +13,7 @@ class DevConfig(BaseConfig):
     DEBUG = True
     if os.environ.get("SQLALCHEMY_DATABASE_URI"):
         SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
 
 class TestingConfig(BaseConfig):
     TESTING = True
