9995b11c433c9682ecaa5cd082c1c4cc1fcd1c53
VladimirGrebenev <grebenev-81@mail.ru>
2023-03-03 12:06:50 +0300

    freeze pip install Flask-WTF, upgrade user model

--- blog/models/user.py
+++ blog/models/user.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, LargeBinary
 from flask_login import UserMixin
+from blog.security import flask_bcrypt
 
 from blog.models.database import db
 
@@ -7,16 +8,22 @@ from blog.models.database import db
 class User(db.Model, UserMixin):
     id = Column(Integer, primary_key=True)
     username = Column(String(80), unique=True, nullable=False)
-    age = Column(Integer)
+    first_name = Column(String(120), unique=False, nullable=False, default="", server_default="")
+    last_name = Column(String(120), unique=False, nullable=False, default="", server_default="")
     email = Column(String(255), unique=True, nullable=False)
-    password = Column(String(255))
+    _password = Column(LargeBinary, nullable=True)
     is_staff = Column(Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<User #{self.id} {self.username!r}>'
 
-    # def __init__(self, username, email, password, is_staff):
-    #     self.username = username
-    #     self.email = email
-    #     self.password = password
-    #     self.is_staff = is_staff
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        self._password = flask_bcrypt.generate_password_hash(value)
+
+    def validate_password(self, password) -> bool:
+        return flask_bcrypt.check_password_hash(self._password, password)
