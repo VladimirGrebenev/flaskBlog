4df5674a11ce6fc70dfa5b75840502730da74417
VladimirGrebenev <grebenev-81@mail.ru>
2023-03-10 16:43:39 +0300

    Add Tag model. Add cross link to all views.

--- blog/app.py
+++ blog/app.py
@@ -51,3 +51,22 @@ def create_admin():
     db.session.add(admin)
     db.session.commit()
     print("created admin:", admin)
+
+@app.cli.command("create-tags")
+def create_tags():
+    """
+    Run in your terminal:
+    ➜ flask create-tags
+    """
+    from blog.models import Tag
+    for name in [
+        "flask",
+        "django",
+        "python",
+        "sqlalchemy",
+        "news",
+    ]:
+        tag = Tag(name=name)
+        db.session.add(tag)
+    db.session.commit()
+    print("created tags")
--- blog/articles/views.py
+++ blog/articles/views.py
@@ -2,12 +2,14 @@ from flask import Blueprint, render_template, request, current_app, redirect, ur
 from flask_login import login_required, current_user
 from sqlalchemy.exc import IntegrityError
 from werkzeug.exceptions import NotFound
+from sqlalchemy.orm import joinedload
+
 from blog.models.database import db
-from blog.models import Author, Article
+from blog.models import Author, Article, Tag, User
 from blog.forms.article import CreateArticleForm
 
-
 articles_app = Blueprint('articles_app', __name__, url_prefix='/articles')
+
 
 @articles_app.route("/", endpoint="list")
 @login_required
@@ -16,9 +18,20 @@ def articles_list():
     return render_template("articles/list.html", articles=articles)
 
 
+@articles_app.route("/tag/<int:tag_id>/", endpoint="list_by_tag")
+@login_required
+def articles_list_by_tag(tag_id):
+    tag = Tag.query.filter_by(id=tag_id).options(joinedload(Tag.articles)).one_or_none()
+    if tag is None:
+        raise NotFound
+    return render_template("articles/articles_by_tag.html", tag=tag)
+
+
 @articles_app.route("/<int:article_id>/", endpoint="details")
 def article_detals(article_id):
-    article = Article.query.filter_by(id=article_id).one_or_none()
+    article = Article.query.filter_by(id=article_id).options(
+        joinedload(Article.tags)  # подгружаем связанные теги!
+    ).one_or_none()
     if article is None:
         raise NotFound
     return render_template("articles/detail.html", article=article)
@@ -29,9 +42,14 @@ def article_detals(article_id):
 def create_article():
     error = None
     form = CreateArticleForm(request.form)
-    if request.method == "POST" and form.validate_on_submit():
+    # добавляем доступные теги в форму
+    form.tags.choices = [(tag.id, tag.name) for tag in Tag.query.order_by("name")]
+    if request.method == "POST" and form.validate_on_submit():  # при создании статьи
         article = Article(title=form.title.data.strip(), body=form.body.data)
-
+        if form.tags.data:  # если в форму были переданы теги (были выбраны)
+            selected_tags = Tag.query.filter(Tag.id.in_(form.tags.data))
+        for tag in selected_tags:
+            article.tags.append(tag)  # добавляем выбранные теги к статье
         if current_user.author:
             # use existing author if present
             article.author = current_user.author
