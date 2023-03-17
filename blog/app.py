1ffe7bf84c774f29db31b1f4b6d357bf7bdaf240
VladimirGrebenev <grebenev-81@mail.ru>
2023-03-17 20:35:06 +0300

    new db, pip freeze

--- blog/app.py
+++ blog/app.py
@@ -20,12 +20,13 @@ cfg_name = os.environ.get("CONFIG_NAME") or "ProductionConfig"
 app.config.from_object(f"blog.configs.{cfg_name}")
 
 # Init apps
+api = init_api(app)
 flask_bcrypt.init_app(app)
 db.init_app(app)
 migrate = Migrate(app, db, compare_type=True)
 login_manager.init_app(app)
 admin_app.init_app(app)
-api = init_api(app)
+
 
 # register_blueprints
 app.register_blueprint(user_app)
